9772b414193cf7c5ce37b0eb863e5a7db38ed2b8
parwiz-hub <parwizyosefi786@gmail.com>
2023-05-24 11:14:40 -0700

    create column and expanded Widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class InputPage extends StatefulWidget {
   @override
   State<InputPage> createState() => _InputPageState();
@@ -8,11 +9,45 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  usableWidget(),
+                  usableWidget(),
+                ],
+              ),
+            ),
+            usableWidget(),
+            Expanded(
+              child: Row(
+                children: [
+                  usableWidget(),
+                  usableWidget(),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class usableWidget extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(14),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Color(0xFF111428),
+        ),
       ),
     );
   }
