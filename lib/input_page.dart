f6ebfc89590818bafc87b6dbb0b6827e25fdff50
parwiz-hub <parwizyosefi786@gmail.com>
2023-05-26 07:49:18 -0700

    Add Foter And Icon

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const B_color = Color(0xFFEB1557);
+const E_color = Color(0xFF111428);
 
 class InputPage extends StatefulWidget {
   @override
@@ -9,36 +13,90 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('BMI CALCULATOR'),
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                usableWidget(
+                  color: E_color,
+                  icon: iconPart(
+                    useIcon: FontAwesomeIcons.mars,
+                    Lable: 'MALE',
+                  ),
+                ),
+                usableWidget(
+                  color: E_color,
+                  icon: iconPart(
+                      useIcon: FontAwesomeIcons.venus, Lable: 'FEMALE'),
+                ),
+              ],
+            ),
+          ),
+          usableWidget(
+            color: E_color,
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                usableWidget(
+                  color: E_color,
+                ),
+                usableWidget(
+                  color: E_color,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 65,
+            color: B_color,
+            margin: EdgeInsets.only(top: 10),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class iconPart extends StatelessWidget {
+  final IconData useIcon;
+  final String Lable;
+  iconPart({required this.useIcon, required this.Lable});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          useIcon,
+          size: 66,
+          color: Color(0xFF868892),
         ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Row(
-                children: [
-                  usableWidget(),
-                  usableWidget(),
-                ],
-              ),
-            ),
-            usableWidget(),
-            Expanded(
-              child: Row(
-                children: [
-                  usableWidget(),
-                  usableWidget(),
-                ],
-              ),
-            ),
-          ],
-        ));
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          Lable,
+          style: TextStyle(
+            fontSize: 15,
+            color: Color(0xFF868892),
+            fontWeight: FontWeight.bold,
+          ),
+        )
+      ],
+    );
   }
 }
 
 class usableWidget extends StatelessWidget {
-  
-
+  usableWidget({required this.color, this.icon});
+  final Color color;
+  final Widget? icon;
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -46,8 +104,9 @@ class usableWidget extends StatelessWidget {
         margin: EdgeInsets.all(14),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
-          color: Color(0xFF111428),
+          color: color,
         ),
+        child: icon,
       ),
     );
   }
